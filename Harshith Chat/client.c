87d943b8bbc1e030f5b214fc8abace7cdc0ef5be
Harshith Kunder <97864909+harshith754@users.noreply.github.com>
2023-02-21 11:03:16 +0530

    Add files via upload

---
+++ Harshith Chat/client.c
@@ -0,0 +1,64 @@
+// programming
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+
+int main(int argc, char const* argv[])
+{
+   int sock = 0, valread, client_fd;
+   struct sockaddr_in serv_addr;
+   printf("Enter the paragraph:\n");
+   char* paragraph ;
+   scanf("%[^\n]%*c", paragraph);
+  
+   char buffer[1024] = { 0 };
+   if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+       printf("\n Socket creation error \n");
+       return -1;
+   }
+
+
+   serv_addr.sin_family = AF_INET;
+   serv_addr.sin_port = htons(PORT);
+
+
+   // Convert IPv4 and IPv6 addresses from text to binary
+   // form
+   if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+       <= 0) {
+       printf(
+           "\nInvalid address/ Address not supported \n");
+       return -1;
+   }
+
+
+   if ((client_fd
+       = connect(sock, (struct sockaddr*)&serv_addr,
+               sizeof(serv_addr)))
+       < 0) {
+       printf("\nConnection Failed \n");
+       return -1;
+   }
+   send(sock, paragraph, strlen(paragraph), 0);
+   printf("Paragraph sent to server\n");
+
+
+   valread = read(sock, buffer, 1024);
+  
+   //printf("%s\n", buffer);
+
+
+   // closing the connected socket
+
+
+   printf("Information returned from server is :\n%s\n",buffer);
+
+
+   close(client_fd);
+   return 0;
+}
+
